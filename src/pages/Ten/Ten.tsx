7afdd20ef0d1208689e6abcd35ef8235ebb00969
Y <y@Ys-MacBook-Pro.lan>
2022-10-27 09:29:26 +0100

    quiz answer animation

--- src/pages/Ten/Ten.tsx
+++ src/pages/Ten/Ten.tsx
@@ -1,4 +1,4 @@
-import { motion } from "framer-motion"
+import { AnimatePresence, motion } from "framer-motion"
 import { useContext, useEffect, useState } from "react"
 import Disclaimer from "../../components/Disclaimer"
 import SlideContext from "../../contexts/SlideContext"
@@ -36,28 +36,31 @@ export default () => {
                                     <div className="relative flex flex-col items-center gap-10">
                                         <h1 className="text-8xl w-[2300px]">{questionText}</h1>
                                         <div className="flex flex-col gap-7 font-bold2">
-                                            {choices.map(choice => {
-                                                const choiceSelected = choice === answer;
-                                                const choiceCorrect = choice === questionAnswer;
+                                            <AnimatePresence mode="wait">
+                                                {choices.map((choice, key) => {
+                                                    const choiceSelected = choice === answer;
+                                                    const choiceCorrect = choice === questionAnswer;
 
-                                                return (
-                                                    <div className="relative">
-                                                        <p className={`${answer === null ? "bg-[#007A33]" : questionAnswer === choice ? "bg-[#007A33]" : answer === choice ? "bg-white text-black" : "bg-[#4D4D4F]"} px-48 w-[2300px] text-4x h-[240px] text-white flex items-center justify-center`}
-                                                        onClick={() => {
-                                                            if(answer === null) {
-                                                                setAnswer(choice);
-                                                                if(choice === questionAnswer) {
-                                                                    setScore(score + 1)
+                                                    return (
+                                                        <motion.div key={key} transition={{delay: 0.3*(key+1)}} initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}}
+                                                        className="relative">
+                                                            <p className={`${answer === null ? "bg-[#007A33]" : questionAnswer === choice ? "bg-[#007A33]" : answer === choice ? "bg-white text-black" : "bg-[#4D4D4F]"} px-48 w-[2300px] text-4x h-[240px] text-white flex items-center justify-center`}
+                                                            onClick={() => {
+                                                                if(answer === null) {
+                                                                    setAnswer(choice);
+                                                                    if(choice === questionAnswer) {
+                                                                        setScore(score + 1)
+                                                                    }
                                                                 }
-                                                            }
-                                                        }}>{choice}</p>
-                                                        { answer !== null && choiceSelected && !choiceCorrect && 
-                                                        <img className="absolute right-14 top-2/4 -translate-y-2/4 mr-5 w-12 aspect-square" src="./images/cross.svg" alt="incorrect"/> }
-                                                        {answer !== null && choiceCorrect &&
-                                                        <img className="absolute right-14 top-2/4 -translate-y-2/4 mr-5 w-12 aspect-square" src="./images/checkmark.svg" alt="correct"/> }
-                                                    </div>
-                                                )
-                                            })}
+                                                            }}>{choice}</p>
+                                                            { answer !== null && choiceSelected && !choiceCorrect && 
+                                                            <img className="absolute right-14 top-2/4 -translate-y-2/4 mr-5 w-12 aspect-square" src="./images/cross.svg" alt="incorrect"/> }
+                                                            {answer !== null && choiceCorrect &&
+                                                            <img className="absolute right-14 top-2/4 -translate-y-2/4 mr-5 w-12 aspect-square" src="./images/checkmark.svg" alt="correct"/> }
+                                                        </motion.div>
+                                                    )
+                                                })}
+                                            </AnimatePresence>
                                         </div>
                                         <div className="absolute -bottom-60 text-[88px] font-bold-ext">
                                             {answer !== null && (questionAnswer === answer
